0a512acf81e423a5f73c016268c6e5e753ddcb86
Brandon Goldsworthy <BGoldsworthy@neumont.edu>
2020-11-21 16:24:20 -0700

    Add project files.

---
+++ Application/Main.cpp
@@ -0,0 +1,83 @@
+#include "pch.h"
+#include <glad\glad.h>
+#include "Engine\Graphics\Renderer.h"
+#include "Engine\Graphics\Program.h"
+#include "Engine/Graphics/Texture.h"
+
+int main(int argc, char** argv)
+{
+	nc::Renderer renderer;
+	renderer.Startup();
+	renderer.Create("OpenGL", 800,400);
+
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.5, 1.0f,
+		0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f
+	};
+
+	nc::Program program;
+	program.CreateShaderFromFile("shaders\\basic.vert", GL_VERTEX_SHADER);
+	program.CreateShaderFromFile("shaders\\basic.frag", GL_FRAGMENT_SHADER);
+	program.Link();
+	program.Use();
+
+	//create vertex buffers
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//set position pipline (vertex attribute)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+	
+	//set color pipline (vertex attribute)
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	
+	//set uv pipline (vertex attribute)
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//uniform
+	glm::mat4 transform = glm::mat4(1.0f);
+	program.SetUniform("transform", transform);
+
+	nc::Texture texture;
+	texture.CreateTexture("textures\\rock.png");
+
+	bool quit = false;
+	while (!quit)
+	{
+		SDL_Event event;
+		SDL_PollEvent(&event);
+
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+			{
+				quit = true;
+			}
+		}
+
+		SDL_PumpEvents();
+
+		transform = glm::rotate(transform, 0.003f, glm::vec3(0,0,1));
+		program.SetUniform("transform", transform);
+
+		renderer.BeginFrame();
+
+		//render triangle
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		renderer.EndFrame();
+	}
+
+	return 0;
+}
